8a888fefa3655db1beff45896180b7850c5f4da2
Ega Fadhilla Febryana <100186624+egafadhillafebry@users.noreply.github.com>
2022-03-29 22:25:56 +0700

    Add files via upload

---
+++ array6.php
@@ -0,0 +1,17 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+    </head>
+    <body>
+    <h2>Fungsi count () </h2>
+        <?php
+            $menu = array ("rawon", "sate", "nasi goreng");
+            $arrLength = count($menu);
+
+            echo "Menu hari ini adalah: <br>";
+            for($x = 0; $x < $arrLength; $x++){     
+            }
+            echo "<br> Saya lapar, saya ingin makan " . "</br";
+        ?>
+    </body>
+</html>
